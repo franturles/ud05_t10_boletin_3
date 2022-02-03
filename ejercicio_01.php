b87af9a25c745a20bb91cbc2a236a3da3e0669ea
franturles <kikofran35@hotmail.com>
2022-02-03 16:30:31 +0000

    Array incorrecto

--- ejercicio_01.php
+++ ejercicio_01.php
@@ -12,32 +12,29 @@ echo "<!DOCTYPE html>
     <body>
         <form action='ejercicio_01.php' enctype='multipart/form-data' method='post'>
             <p>Dime un dia de la semana: <input type='number' name='numerosem'/></p>
-            <p><input type='submit' value='Submit'/></p>
-        </form>
-     </body>";
-     $nomeCompleto=htmlspecialchars(trim(strip_tags($_REQUEST['nomeCompleto'])), ENT_QUOTES, "ISO-8859-1");
-$Dia=htmlspecialchars(trim(strip_tags($_REQUEST['numerosem'])),ENT_QUOTES, "ISO-8859-1");
-
-$horario=array(
-    '8:50 - 9:40' => array('IAW','SAD','ASX','ASX','IAW'),
-    '9:40 - 10:30' => array('IAW','SAD','EIEA','ASX','SRI'),
-    '10:30 - 11:20' => array('SRI','SAD','EIEA','EIEA','SRI'),
-    '11:20 - 12:00' => array('Recreo'),
-    '12:00 - 12:50' => array('SRI','ASO','ASO','IAW','SAD'),
-    '12:50 - 13:40' => array('ASO','ASO','IAW','IAW','SAD'),
-    '13:40 - 14:30' => array('ASX','ASO','IAW','SRI','SAD'),
-    '14:30 - 16:00' => array('KEBAB'),
-    '16:00 - 16:50' => array('','SRI','','ASO',''),
-    '16:50 - 17:40' => array('','SRI','','ASO',''),
-    '17:40 - 18:60' => array('','SRI','','ASO','')
-);
-$Semana=array(
-    'Lunes' => array('IAW','IAW','IAW')
-);
-foreach ($Semana as $Dia => $Primera) {
-    if($Dia = 1){
-        echo "Ese dia tenemos: $Semana[1]";
+            <p><input type='submit' value='Submit'/></p>";
+     $Semana=array(
+        'Lunes' => array('IAW','IAW','IAW'),
+        'Martes' => array('IAW','SAD','SAD'),
+        'Miercoles' => array('ASX','EIEA','EIEA'),
+        'Jueves' => array('ASX','ASX','EIEA'),
+        'Viernes' => array('IAW','SRI','SRI'),
+    );
+$Numero=isset($_REQUEST['numerosem']);
+if ($Numero > 5) {
+    echo "El numero tiene que ser entre 1 y 5";    
+}else{
+    $cont = 1;
+    foreach ($Semana as $Dia => $horario_dia) {
+        if($cont == $Numero){
+            echo $horario_dia[1][$Dia];
+        }
+        $cont++;
+        
     }
-
+        
 }
+echo '</form>
+</body>
+</html>';
 ?>
