4d6284fddc874f3c60089ad69ab6ed0a444cca12
franturles <kikofran35@hotmail.com>
2022-02-20 13:18:22 +0000

    Corregir

---
+++ ejercicio_14.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+    <title>UD05_T10_boletin_3</title>
+        <meta charset='UTF-8'>
+        <h1>UD05_T10</h1>
+        <h2>Ejercicio 14</h2>
+        <h3>Realiza un programa que diga si un número introducido por teclado es par y/o divisible entre 5.</h3>
+    </head>
+    <body>
+    <form action="ejercicio_14.php" enctype='multipart/form-data' method='post'>
+        <p>Numero<input type='number' name='numero' min='0'/></p>
+        <p><input type='submit' value='Submit'/></p>
+    </from>
+    <?php
+    $Numero=$_REQUEST["numero"];
+    if($Numero%2 == 0){
+        echo "$Numero es par";
+    }elseif ($Numero%5 == 0) {
+        echo "$Numero es divisible entre 5";
+    } 
+    
+    ?>
+    </body>
+</html>
